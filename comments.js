573f1b1df231d28c53916c0fcf681a53a3e63889
Gary <astarmist@hotmail.com>
2023-07-20 05:05:59 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,21 @@
+// Create web server
+// 1. Create web server
+// 2. Create dynamic web server
+// 3. Create dynamic web server with template
+// 4. Create dynamic web server with template and database
+// 5. Create dynamic web server with template and database with pagination
+// 6. Create dynamic web server with template and database with pagination and session
+// 7. Create dynamic web server with template and database with pagination and session and authentication
+// 8. Create dynamic web server with template and database with pagination and session and authentication and RESTful API
+
+// 8. Create dynamic web server with template and database with pagination and session and authentication and RESTful API
+// 8.1. Create dynamic web server with template and database with pagination and session and authentication and RESTful API and file upload
+// 8.2. Create dynamic web server with template and database with pagination and session and authentication and RESTful API and file upload with multer
+// 8.3. Create dynamic web server with template and database with pagination and session and authentication and RESTful API and file upload with multer and sharp
+// 8.4. Create dynamic web server with template and database with pagination and session and authentication and RESTful API and file upload with multer and sharp and validator
+// 8.5. Create dynamic web server with template and database with pagination and session and authentication and RESTful API and file upload with multer and sharp and validator and sanitize
+// 8.6. Create dynamic web server with template and database with pagination and session and authentication and RESTful API and file upload with multer and sharp and validator and sanitize and helmet
+// 8.7. Create dynamic web server with template and database with pagination and session and authentication and RESTful API and file upload with multer and sharp and validator and sanitize and helmet and rate limit
+// 8.8. Create dynamic web server with template and database with pagination and session and authentication and RESTful API and file upload with multer and sharp and validator and sanitize and helmet and rate limit and bcrypt
+// 8.9. Create dynamic web server with template and database with pagination and session and authentication and RESTful API and file upload with multer and sharp and validator and sanitize and helmet and rate limit and bcrypt and mongoose
+// 8.10. Create dynamic web server with template and database with pagination and session and authentication and RESTful API and file upload with multer and sharp and validator and
